05d1022b559f9642cea3c5e69ee3786c6c4fc5a3
Arlind Hajzeraj <arlindhajzeraj20@gmail.com>
2025-03-16 15:24:28 +0100

    addded wwwroot

---
+++ wwwroot/js/main.js
@@ -0,0 +1,22 @@
+﻿async function fetchZones() {
+    const response = await fetch('/GetZones'); 
+    const zones = await response.json();
+
+    const zoneList = document.getElementById('zoneList');
+    
+
+    zoneList.innerHTML = '';
+
+    zones.forEach(zone => {
+        if (zones.length > 0) {
+            const li = document.createElement('li');
+            li.textContent = `Zone: ${zone.zoneName}, Price: $${zone.pricePerHour}/hr`;
+            zoneList.appendChild(li);
+
+        }
+    });
+}
+
+
+
+
